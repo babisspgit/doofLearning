2233f5b4e15ca1fdeaf9a26842fbb28c08840888
manganas <manganas.ioannis@gmail.com>
2023-10-27 22:29:09 +0200

    initial dataset to test how it works

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,46 +1,85 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
+
 # from dotenv import find_dotenv, load_dotenv
 from torch.utils.data import Dataset
 from PIL import Image
 import pandas as pd
 
-class DatasetTitleOnly(Dataset):
-    def __init__(self, image_path, csv_path, transformations=None):
-        super(DatasetTitleOnly, self).__init__()
+from torchvision import transforms
+import torchtext
 
-        self.recipes_df = pd.read_csv(csv_path) # For now load everything
 
-        if transformations:
+class DatasetRecipes(Dataset):
+    def __init__(self, data_path, transformations=None):
+        super(DatasetRecipes, self).__init__()
+
+        self.MAX_SEQ_LEN = 512
+        self.VOCAB_SIZE = 50_000
+
+        csv_path = (
+            Path(data_path)
+            / "Food Ingredients and Recipe Dataset with Image Name Mapping.csv"
+        )
+
+        self.recipes_df = pd.read_csv(csv_path)  # For now load everything
+        self.image_path = Path(data_path) / "Food Images" / "Food Images"
+
+        if not transformations:
+            self.transformations = transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5,), (0.5,)),
+                ]
+            )
+        else:
             self.transformations = transformations
+
+    def tokenize(self, input_text):
+        TEXT = torchtext.data.Field(lower=True, include_lengths=True, batch_first=True)
+        TEXT.build_vocab(input_text, max_size=self.VOCAB_SIZE - 2)
+        return input_text
 
     def __len__(self):
         return len(self.recipes_df)
 
     def __getitem__(self, idx):
-        img = Image.open(self.image_names_list[idx])
+        data_point = self.recipes_df.iloc[idx]
 
-        # Only get the title
-        recipe_title = self.recip
+        # Prepare the text data
+
+        title = data_point.Title
+        ingredients = data_point.Cleaned_Ingredients  # Look the same as Ingredients
+        instruction = data_point.Instructions
+
+        # Prepare the image
+        image_name = data_point.Image_Name + ".jpg"
+        image_path = self.image_path / image_name
+        img = Image.open(image_path)
+
+        return title, self.tokenize(title)
+
+        return self.transformations(img), title, instruction
 
 
-
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def main(input_filepath):
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
+
+    dataset = DatasetRecipes(input_filepath)
+
+    i = 10
+    a = dataset[i]
+
+    print(a)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
@@ -48,6 +87,9 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
+
+    input_filepath = r"data/raw/archive"
+    output_filepath = r"data/processed"
     # load_dotenv(find_dotenv())
 
-    main()
+    main(input_filepath)
