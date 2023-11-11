5245e3f4dc368efa837bdf2c5bbb9efa44a2298c
manganas <manganas.ioannis@gmail.com>
2023-11-11 17:33:01 +0100

    changed ViT_Text to output embeddings and scaled cosines

--- src/models/models.py
+++ src/models/models.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
-from src.models.Transformers_ import ViT, TextTransformer,bert_model
+from src.models.Transformers_ import ViT, TextTransformer, bert_model
 from src.models.CNN_models import VGG, VGGImageEncoder
+
 
 ## Vit + TextTransformer
 class TransformersSingleTextModel(nn.Module):
@@ -16,7 +17,6 @@ class TransformersSingleTextModel(nn.Module):
         self.img_model = ViT(**img_options_dict)
         self.text_model = TextTransformer(**text_options_dict)
 
-        return img_out, text_out
         # Magic or learnable number? Found in clip
         if logit_scale_init_value:
             self.logit_scale = nn.Parameter(torch.tensor(logit_scale_init_value))
@@ -35,12 +35,13 @@ class TransformersSingleTextModel(nn.Module):
 
         # Create the cosine similarities as a matrix
         logit_scale = self.logit_scale.exp()
+
         logits_per_text = (
             torch.matmul(text_embeddings, img_embeddings.t()) * logit_scale
         )
         logits_per_image = logits_per_text.t()
 
-        return logits_per_text, logits_per_image
+        return logits_per_text, logits_per_image, text_embeddings, img_embeddings
 
 
 class CLIP_(nn.Module):
@@ -51,7 +52,8 @@ class CLIP_(nn.Module):
 
     def forward(self, img, text):
         return self.clip_model(img, text)
-    
+
+
 ## Vit + bert
 class Transformers_Bert(nn.Module):
     def __init__(self, img_options_dict: dict, text_options_dict: dict) -> None:
@@ -59,68 +61,70 @@ class Transformers_Bert(nn.Module):
 
         self.img_model = ViT(**img_options_dict)
         self.text_model = bert_model()
-        
+
     def forward(self, img_tensor, tok_text_tensor):
         img_out = self.img_model(img_tensor)
         text_out = self.text_model(tok_text_tensor)
 
         return img_out, text_out
-    
+
 
 ## VGG + TextTransformer
 class VGG_SingleTextModel(nn.Module):
     def __init__(self, img_options_dict: dict, text_options_dict: dict) -> None:
         super().__init__()
-        
+
         self.img_model = VGG()
         self.text_model = TextTransformer(**text_options_dict)
-        
+
     def forward(self, img_tensor, tok_text_tensor):
         img_out = self.img_model(img_tensor)
         text_out = self.text_model(tok_text_tensor)
 
         return img_out, text_out
-    
+
+
 # pretrained VGG
 class VGGpre_SingleTextModel(nn.Module):
     def __init__(self, img_options_dict: dict, text_options_dict: dict) -> None:
         super().__init__()
-        
+
         # Define the output dimension for your image embeddings
         output_dim = 512  # Example dimension size
         self.img_model = VGGImageEncoder(output_dim=output_dim)
         self.text_model = TextTransformer(**text_options_dict)
-        
+
     def forward(self, img_tensor, tok_text_tensor):
         img_out = self.img_model(img_tensor)
         text_out = self.text_model(tok_text_tensor)
 
         return img_out, text_out
-    
 
-## VGG + bert    
+
+## VGG + bert
 class VGG_Bert(nn.Module):
     def __init__(self, img_options_dict: dict, text_options_dict: dict) -> None:
         super().__init__()
 
         self.img_model = VGG(**img_options_dict)
         self.text_model = bert_model(**text_options_dict)
-        
+
     def forward(self, img_tensor, tok_text_tensor):
         img_out = self.img_model(img_tensor)
         text_out = self.text_model(tok_text_tensor)
 
         return img_out, text_out
-    
-# pretrained   
+
+
+# pretrained
 class VGGpre_Bert(nn.Module):
     def __init__(self, img_options_dict: dict, text_options_dict: dict) -> None:
         super().__init__()
 
-        output_dim = 512    
+        output_dim = 512
         self.img_model = VGGImageEncoder(output_dim=output_dim)
         self.text_model = bert_model()
-        
+
     def forward(self, img_tensor, tok_text_tensor):
         img_out = self.img_model(img_tensor)
         text_out = self.text_model(tok_text_tensor)
